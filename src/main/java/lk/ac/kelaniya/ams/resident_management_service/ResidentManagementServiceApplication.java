2806845780c41c2f261b440eb6ff10d1fd204339
salahmohammed05 <salahmohammed0385@gmail.com>
2026-09-12 22:20:09 +0530

    feat: initialize resident management service

---
+++ src/main/java/lk/ac/kelaniya/ams/resident_management_service/ResidentManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.ac.kelaniya.ams.resident_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResidentManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResidentManagementServiceApplication.class, args);
+    }
+
+}
